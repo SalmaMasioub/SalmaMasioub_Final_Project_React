9f144b731992f19f1ec9f314882aa13579603372
SalmaMasioub <salma.masioub6@gmail.com>
2025-06-02 19:42:57 +0100

    first commit

---
+++ fashe/src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeCarousel from './components/caroussel';
+
+const Home = () => {
+    return (
+        <div>
+        <HomeCarousel></HomeCarousel> 
+        </div>
+    );
+};
+
+export default Home;
