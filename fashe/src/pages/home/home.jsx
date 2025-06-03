54b2dd5eaeafd4746e677d8b435af8294b0cb8fc
SalmaMasioub <salma.masioub6@gmail.com>
2025-06-03 10:45:16 +0100

    add featured products section

--- fashe/src/pages/home/home.jsx
+++ fashe/src/pages/home/home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Product from './components/product';
 import AnimatedSlide from './components/caroussel';
+import Carousel from './components/featuredProducts';
 
 const Home = () => {
     return (
         <div>
         <AnimatedSlide></AnimatedSlide> 
         <Product></Product>
+        <Carousel></Carousel>
         </div>
     );
 };
