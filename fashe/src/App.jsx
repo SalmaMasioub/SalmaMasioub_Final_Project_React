3a869a50c9cf8506b062ffd801e587d03e0133de
SalmaMasioub <salma.masioub6@gmail.com>
2025-06-04 15:47:50 +0100

    add the contact page

--- fashe/src/App.jsx
+++ fashe/src/App.jsx
@@ -9,30 +9,31 @@ import Blogdetails from './layout/blogdetails';
 import Footer from './layout/footer';
 import Navbar from './layout/navbar';
 import About from './pages/about/about';
+import Contact from './pages/contact/contact';
 
 const App = () => {
-    return (
-        <ProductProvider>
-            <div className='flex'>
-                <div className='lg:w-[25vw] h-1/2 bg-amber-400'>
-                    <Navbar></Navbar>
-                </div>
+  return (
+    <ProductProvider>
+      <div className='flex'>
+        <div className='lg:w-[25vw] h-1/2 bg-amber-400'>
+          <Navbar></Navbar>
+        </div>
 
-                <div className="w-full lg:w-[75vw] flex flex-col justify-start items-center min-h-[100px]">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                                                <Route path="/about" element={<About />} />
+        <div className="w-full lg:w-[75vw] flex flex-col justify-start items-center min-h-[100px]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+<Route path="/contact" element={<Contact />} />
+            <Route path="/product" element={<Shop />} />
+            <Route path="/details/:title" element={<Details />} />
+            <Route path="/blogs/:name" element={<Blogdetails />} />
+          </Routes>
 
-                        <Route path="/product" element={<Shop />} />
-                        <Route path="/details/:title" element={<Details />} />
-                        <Route path="/blogs/:name" element={<Blogdetails />} />
-                    </Routes>
-
-                    <Footer />
-                </div>
-            </div>
-        </ProductProvider>
-    );
+          <Footer />
+        </div>
+      </div>
+    </ProductProvider>
+  );
 };
 
 export default App;
