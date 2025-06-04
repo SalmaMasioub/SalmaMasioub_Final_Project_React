ac927353009f466cf43ca9a82df63e0c44375f31
SalmaMasioub <salma.masioub6@gmail.com>
2025-06-04 11:43:28 +0100

    add cover to products

--- fashe/src/pages/shop/components/cover.jsx
+++ fashe/src/pages/shop/components/cover.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Coverproduct = () => {
-    return (
-        <div className='w-[75vw] flex items-center justify-center'>
-<h1 className="text-center font-bold w-full h-60 bg-amber-300">PRODUCT</h1>
-        </div>
-      
-    );
+  return (
+    <div 
+      className="w-[75vw] h-60 flex justify-center items-center bg-[url('/src/assets/images/banner.png')] bg-cover"
+    >
+      <h1 className="text-white text-5xl font-bold">PRODUCT</h1>
+    </div>
+  );
 };
+
 
 export default Coverproduct;
